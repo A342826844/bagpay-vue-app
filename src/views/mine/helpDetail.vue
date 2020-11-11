6138d6447cfe459a9698668aa281255b8b2cdee7
aixiaolong <342826844@qq.com>
2020-11-11 11:36:54 +0800

    feat: 视频封面

--- src/views/mine/helpDetail.vue
+++ src/views/mine/helpDetail.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="mine-help-detail">
         <Headers :title="$t(title)"></Headers>
-        <video webkit-playsinline data-setup='{}' :src="video[id]" controls class="video-js" >
-            <source :src="video[id]" type="video/mp4"/>
+        <video webkit-playsinline data-setup='{}' :src="video[id].src" :poster="video[id].poster" controls class="video-js" >
+            <source :src="video[id].src" type="video/mp4"/>
             不支持视频播放
         </video>
         <!-- <video class="video" preload src="http://x100.oss-ap-southeast-1.aliyuncs.com/wap-bagpay/video/login.mp4" controls>
@@ -36,6 +36,9 @@ type data = {
 // const login = require('../../assets/video/login.mp4');
 // const tranfer = require('../../assets/video/tranfer.mp4');
 // const otc = require('../../assets/video/otc.mp4');
+const login = require('../../assets/img/mine/help-login.jpg');
+const tranfer = require('../../assets/img/mine/help-tranfer.jpg');
+const otc = require('../../assets/img/mine/help-otc.jpg');
 
 export default Vue.extend({
     name: 'SetPaymentAdd',
@@ -44,9 +47,18 @@ export default Vue.extend({
             id: '1',
             title: '',
             video: {
-                1: 'https://x100.oss-ap-southeast-1.aliyuncs.com/wap-bagpay/video/register.mp4',
-                2: 'https://x100.oss-ap-southeast-1.aliyuncs.com/wap-bagpay/video/tranfer.mp4',
-                3: 'https://x100.oss-ap-southeast-1.aliyuncs.com/wap-bagpay/video/otc.mp4',
+                1: {
+                    src: 'https://x100.oss-ap-southeast-1.aliyuncs.com/wap-bagpay/video/register.mp4',
+                    poster: login,
+                },
+                2: {
+                    src: 'https://x100.oss-ap-southeast-1.aliyuncs.com/wap-bagpay/video/tranfer.mp4',
+                    poster: tranfer,
+                },
+                3: {
+                    src: 'https://x100.oss-ap-southeast-1.aliyuncs.com/wap-bagpay/video/otc.mp4',
+                    poster: otc,
+                },
             },
         };
     },
