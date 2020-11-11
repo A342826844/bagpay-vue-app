d15bde7429f260c9a8b9042b07779c21d1182ad6
aixiaolong <342826844@qq.com>
2020-11-11 13:54:17 +0800

    feat: app更新

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,9 @@
             </keep-alive>
             <router-view v-if="!$route.meta.keepAlive"></router-view>
         </div>
+        <van-dialog v-model="show" :title="force_update ? $t('mine.forceUpdate') : $t('mine.update')" :show-confirm-button="!force_update">
+            <van-circle v-model="rate" :rate="progress" :text="toastOperateTitle" />
+        </van-dialog>
         <Loading/>
         <Footer v-show="$route.meta.showFooter"/>
     </div>
@@ -25,6 +28,11 @@ export default Vue.extend({
     data() {
         return {
             total: 0,
+            progress: 0,
+            rate: 0,
+            show: false,
+            force_update: false,
+            toastOperateTitle: '',
         };
     },
     created() {
@@ -65,6 +73,7 @@ export default Vue.extend({
                 setTimeout(() => {
                     (window as any).plus.navigator.closeSplashscreen();
                 }, 200);
+                this.saveHandle();
             };
             if ((window as any).plus) {
                 plusReady();
@@ -96,6 +105,73 @@ export default Vue.extend({
                     }
                     this.init();
                 }
+            });
+        },
+        saveHandle() {
+            this.$api.version({
+                channel: ((window as any).plus.os.name || '').toLowerCase(),
+                build: (window as any).plus.runtime.versionCode,
+                version: (window as any).plus.runtime.version,
+            }).then((res: any) => {
+                if (res.code === 0) {
+                    if (!res.data) return;
+                    if (res.data.force_update) {
+                        this.show = true;
+                        this.force_update = true;
+                        this.uploadApp(res.data.url);
+                    } else {
+                        this.$dialog.confirm({
+                            title: `${this.$t('mine.updateV')}`,
+                            message: `${this.$t('mine.updateTip')}`,
+                        }).then(() => {
+                            this.show = true;
+                            this.uploadApp(res.data.url);
+                        });
+                    }
+                }
+            }).finally(() => {
+                this.changeLoading(false);
+            });
+        },
+        iosDown(url: string) {
+            window.open(url);
+        },
+        downloadHandle(url: string, name?: string) {
+            const a = document.createElement('a');
+            a.style.display = 'none';
+            document.body.appendChild(a);
+            a.download = name || `${this.$app_mark}.apk`;
+            a.href = url;
+            a.click();
+            document.body.removeChild(a);
+        },
+        uploadApp(downlaodUrl: string) {
+            if ((window as any).plus.os.name === 'Android') {
+                this.downlaodApp(downlaodUrl);
+            } else {
+                (window as any).plus.runtime.openURL(downlaodUrl, () => {
+                    this.$normalToast('更新失败,请开启浏览器权限');
+                });
+            }
+        },
+        downlaodApp(downlaodUrl: string) {
+            const dtask = (window as any).plus.downloader.createDownload(downlaodUrl, {}, (d: { filename: any }, status: number) => {
+                if (status === 200) {
+                    (window as any).plus.runtime.install(d.filename, null, () => {
+                    // window.plus.runtime.quit();
+                        this.show = false;
+                    }, () => {
+                        this.$normalToast(this.$t('mine.installFailed'));
+                        this.show = false;
+                    });
+                }
+            });
+            dtask.start();
+            dtask.addEventListener('statechanged', (download: { downloadedSize: number; totalSize: number }) => {
+                // eslint-disable-next-line no-mixed-operators
+                const temp = download.downloadedSize / download.totalSize * 100;
+                this.progress = temp ? Number(temp.toFixed(2)) : 0;
+                this.toastOperateTitle = `${this.$t('mine.downloading')} ${this.progress}%`;
             });
         },
     },
