cd752ea1a8b1e614b9439a6af0d3ef0ca3cee91f
aixiaolong <342826844@qq.com>
2021-01-05 17:34:48 +0800

    feat: 购买card 样式优化

--- src/views/otc/component/GoodsCard.base.vue
+++ src/views/otc/component/GoodsCard.base.vue
@@ -7,29 +7,15 @@
                         <h5 class="name">{{ renderData.nickname }}</h5>
                         <img v-if="arrow" class="app-img-50" src="@/assets/img/common/arrow_right1.png" alt="">
                     </div>
-                    <!-- <p class="otc-good-card-pay value">{{ renderData.pay_types | payType}}</p> -->
+                    <p class="otc-good-card-pay value">{{ renderData.pay_types | payType}}</p>
                 </div>
                 <div class="text-align-r">
-                    <!-- <p class="lable" v-t="'otc.unitPrice'"></p>
-                    <h6 class="app-size-45 primary-color otc-good-card-price">${{ renderData.price }}</h6> -->
-                    <span class="lable">{{$t('otc.quota')}}($)：</span>
-                    <h6 style="display: inline">{{ renderData.min_value }}~{{ renderData.max_value }}</h6>
+                    <p class="lable" v-t="'otc.unitPrice'"></p>
+                    <h6 class="app-size-45 primary-color otc-good-card-price">${{ renderData.price }}</h6>
                 </div>
             </div>
             <div class="flex-between-c otc-good-card-num">
-                <div class="text-align-l">
-                    <p class="lable">{{$t('otc.num')}}({{renderData.coin.toUpperCase()}})</p>
-                    <h6 class=" app-size-34 otc-good-card-value">
-                        {{ Number((renderData.total - renderData.filled - renderData.frozen).toFixed(4)) }}
-                    </h6>
-                </div>
-                <div class="text-align-r">
-                    <p class="lable">{{$t('otc.unitPrice')}}($)</p>
-                    <h6 class="primary-color app-size-34 otc-good-card-value">{{renderData.price}}</h6>
-                </div>
-            </div>
-            <div class="flex-between-c otc-good-card-num">
-                <!-- <div class="text-align-l value">
+                <div class="text-align-l value">
                     <div>
                         <span class="lable" v-t="'otc.num'"></span>
                         {{ Number((renderData.total - renderData.filled - renderData.frozen).toFixed(4)) }}  {{renderData.coin.toUpperCase()}}
@@ -37,9 +23,6 @@
                     <div>
                         <span class="lable" v-t="'otc.quota'"></span> ${{ renderData.min_value }}~{{ renderData.max_value }}
                     </div>
-                </div> -->
-                <div>
-                    <img class="app-img-50" :src="PayTypeImg[renderData.pay_types]" alt="">
                 </div>
                 <div>
                     <Button
@@ -61,7 +44,6 @@
 
 <script lang="ts">
 import Vue from 'vue';
-import { PayTypeImg } from '@/commons/config';
 
 export default Vue.extend({
     name: 'OtcGardCard',
@@ -79,20 +61,14 @@ export default Vue.extend({
             required: true,
         },
     },
-    data() {
-        return {
-            PayTypeImg,
-        };
-    },
 });
 </script>
 
 <style scoped lang="less">
 @import '../../../assets/less/color.less';
 .otc-good-card{
-    // height: 269px;
-    padding-top: 28px;
-    padding-bottom: 28px;
+    height: 269px;
+    padding-top: 20px;
     .lable{
         color: #A5A5A5;
     }
@@ -104,9 +80,6 @@ export default Vue.extend({
         font-size: 28px;
         color: #343434;
     }
-    &-value{
-        margin-top: 8px;
-    }
     &-pay{
         margin-top: 8px;
     }
@@ -114,8 +87,8 @@ export default Vue.extend({
         margin-top: 15px;
     }
     &-num{
-        margin-top: 15px;
-        // line-height: 50px;
+        margin-top: 22px;
+        line-height: 50px;
     }
 }
 </style>
